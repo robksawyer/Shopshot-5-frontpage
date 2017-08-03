1e28dadc943ad073daba152ee8e73bf949a217cd
rsawyer <rsawyer@tripwire.com>
2017-08-03 11:10:23 -0700

    Added some new config options to keystone file.

--- src/server/keystone.js
+++ src/server/keystone.js
@@ -1,24 +1,53 @@
 // Simulate config options from your production environment by
 // customising the .env file in your project's root folder.
-const config = require('./config/vars');
+require('dotenv').config('./config');
+
+// Initialise New Relic if an app name and license key exists
+if (process.env.NODE_ENV === 'production'){
+	if(process.env.NEW_RELIC_APP_NAME && process.env.NEW_RELIC_LICENSE_KEY) {
+		require('newrelic');
+	}
+}
 
 // Require keystone
 var keystone = require('keystone');
+var pkg = require('./package.json');
+
+// Handle finding the right database to use.
+// Builds a database based on the name in package.json.
+// Warning: You shouldn't have any spaces or weird characters in the name.
+var mongoUrl = process.env.MONGO_URI;
+if(process.env.NODE_ENV === 'local') {
+	if(process.env.USE_LIVE_DB === 'true') {
+		mongoUrl = process.env.MONGO_URI;
+	} else {
+		mongoUrl = 'mongodb://localhost/' + pkg.name;
+	}
+}
 
 // Initialise Keystone with your project's configuration.
 // See http://keystonejs.com/guide/config for available options
 // and documentation.
 
 keystone.init({
-	'name': 'shopshot',
-	'brand': 'shopshot',
+
+	'name': 'keystone-react-template',
+	'brand': 'Keystone React Template',
+
 	'mongo': config.MONGO_URI,
 
 	'sass': 'public',
 	'static': ['../../build', 'public'],
 	'favicon': 'public/favicon.ico',
 	'views': 'templates/views',
-	'view engine': 'jade',
+	'view engine': 'pug',
+
+	// Handy options
+	// 'emails': 'templates/emails',
+	// 'admin path': 'admin',
+	// 'signin logo': ['/images/logo.svg', 120],
+
+	'mongo': mongoUrl,
 
 	'auto update': true,
 	'session': true,
