ccbf2e1fc7c36a71b8f74e232a6028c02b568853
rsawyer <rsawyer@tripwire.com>
2017-08-04 10:52:09 -0700

    Added some comments.

--- src/server/routes/react/index.js
+++ src/server/routes/react/index.js
@@ -1,38 +1,27 @@
 import React from 'react';
-import ReactDom from 'react-dom/server';
-import {
-  match
-}
-from 'react-router';
-import {
-  Provider
-}
-from 'react-redux';
-import {
-  ReduxAsyncConnect,
-  loadOnServer
-}
-from 'redux-connect';
+import ReactDOMServer from 'react-dom/server'
+import { match } from 'react-router';
+import { Provider } from 'react-redux';
+import { ReduxAsyncConnect, loadOnServer } from 'redux-connect';
 import configureStore from '../../../client/redux/configureStore';
 import renderHTML from './helpers/render_html';
 import Routes from '../../../client/routes';
 
 const reactModule = (req, res, next) => {
-    const store = configureStore();
 
+    const store = configureStore();
     const state = store.getState();
-    
+
     // This setting is required for material-ui server-side rendering
     //state.theme.userAgent = req.headers['user-agent'];
-    
-    match({
-      routes: Routes,
-      location: req.url
-    }, (error, redirectLocation, renderProps) => {
+
+    // @see https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/match.md
+    // @see https://github.com/makeomatic/redux-connect
+    // 1. load data
+    match({ routes: Routes, location: req.url }, (error, redirectLocation, renderProps) => {
       if (redirectLocation) {
         return res.redirect(301, redirectLocation.pathname + redirectLocation.search);
       }
-
       if (error) {
         return next({
           message: error.message,
@@ -49,12 +38,17 @@ const reactModule = (req, res, next) => {
         ...renderProps,
         store
       }).then(() => {
-        const componentHTML = ReactDom.renderToString(
+
+        // 2. use `ReduxAsyncConnect` instead of `RoutingContext` and pass it `renderProps`
+        const componentHTML = ReactDOMServer.renderToString(
           <Provider store={store} key="provider">
             <ReduxAsyncConnect {...renderProps} />
           </Provider>
         );
-        res.send(renderHTML(componentHTML, store.getState()));
+
+        // 3. render the Redux initial data into the server markup
+        const html = renderHTML(componentHTML, store.getState());
+        res.send(html);
       });
     });
 };
