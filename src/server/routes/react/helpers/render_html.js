47852c7592ead8b07248e830ccbbd403727022e4
rsawyer <rsawyer@tripwire.com>
2017-08-04 13:05:13 -0700

    More tweaks.

--- src/server/routes/react/helpers/render_html.js
+++ src/server/routes/react/helpers/render_html.js
@@ -3,14 +3,14 @@ const renderHTML = (title, componentHTML, initialState) => {
     <!DOCTYPE html>
       <html lang="en-US" style="height: 100%;">
       <head>
-          <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>${title}</title>
-          <link rel="icon" href="/favicon.png">
-          <link href='//fonts.googleapis.com/css?family=Roboto:400,300,500' rel='stylesheet' type='text/css'>
-          <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css"></link>
-          <!-- <link rel="stylesheet" href="/public/css/styles.css"> -->
+          <link rel="icon" href="/favicon.png" />
+          <link href="//fonts.googleapis.com/css?family=Roboto:400,300,500" rel="stylesheet" type="text/css" />
+          <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+          <link href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css" rel="stylesheet" type="text/css" />
+          <!-- <link href="/public/css/styles.css" rel="stylesheet"  /> -->
       </head>
       <body style="position: relative; min-height: 100%;">
         <!-- Bug fix: Extra div fixes issue https://stackoverflow.com/questions/33521047/warning-react-attempted-to-reuse-markup-in-a-container-but-the-checksum-was-inv -->
